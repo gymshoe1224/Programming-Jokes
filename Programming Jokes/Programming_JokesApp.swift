1992231bc9831a898a9db68f4b4a22939979d609
Chris Markiewicz <christopher.e.markiewicz@gmail.com>
2023-02-07 09:25:13 -0600

    Initial Commit

---
+++ Programming Jokes/Programming_JokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programming_JokesApp.swift
+//  Programming Jokes
+//
+//  Created by Chris Markiewicz on 2/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Programming_JokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
